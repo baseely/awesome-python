d00e1e19c79bf01e579658afd0db4d737121bb0e
Shady Baseely <75696750+baseely@users.noreply.github.com>
2024-03-18 23:58:27 +0200

    Update sort.py:

--- sort.py
+++ sort.py
@@ -17,7 +17,7 @@
 def sort_blocks():
     # First, we load the current README into memory
     with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.read()
+        read_me = read_me_file.readlines()
 
     # Separating the 'table of contents' from the contents (blocks)
     table_of_contents = ''.join(read_me.split('- - -')[0])
