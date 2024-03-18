d6d374c4e9d27b27355583020e680827b1685f33
Shady Baseely <75696750+baseely@users.noreply.github.com>
2024-03-18 23:59:31 +0200

    Update sort.py
    - Revert back to the 'read()' function

--- sort.py
+++ sort.py
@@ -17,7 +17,7 @@
 def sort_blocks():
     # First, we load the current README into memory
     with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.readlines()
+        read_me = read_me_file.read()
 
     # Separating the 'table of contents' from the contents (blocks)
     table_of_contents = ''.join(read_me.split('- - -')[0])
